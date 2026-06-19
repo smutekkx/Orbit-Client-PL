568c2229b3b86ceffd9e97b9a5adbfb2a37de818
smutekkx <smutekpilka@gmail.com>
2026-06-19 14:33:42 +0200

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- Orbit Client.py
+++ Orbit Client.py
@@ -33,7 +33,7 @@ from tkinter import filedialog, messagebox
 import customtkinter as ctk
 import minecraft_launcher_lib
 
-VERSION_LAUNCHER = "1.7"
+VERSION_LAUNCHER = "1.8"
 UPDATE_URL = "https://raw.githubusercontent.com/smutekkx/Orbit-Client-PL/refs/heads/main/Orbit%20Client.py"
 
 BASE_FOLDER = os.path.expanduser("~/.orbit_client")
@@ -172,8 +172,15 @@ class OrbitLunarLauncher(ctk.CTk):
         self.nick_var.trace_add("write", self._on_nick_changed)
         
         self.build_launcher_interface()
-
-   def check_hwid_and_notify(self):
+def __init__(self):
+        super().__init__()
+        
+        # DODAJ TĘ LINIĘ:
+        self.check_hwid_and_notify() 
+        
+        # ... reszta Twojego kodu ..
+   
+def check_hwid_and_notify(self):
         print("DEBUG: Rozpoczynam sprawdzanie HWID...") # To pokaże czy funkcja startuje
         try:
             cmd = "powershell -Command \"(Get-CimInstance Win32_ComputerSystemProduct).UUID\""
