d2a6915acef1d791e9c505053363aff8c4104413
smutekkx <smutekpilka@gmail.com>
2026-06-19 13:38:13 +0200

    Reformat and clean up Orbit Client.py

--- Orbit Client.py
+++ Orbit Client.py
@@ -16,7 +16,7 @@ import minecraft_launcher_lib
 VERSION_LAUNCHER = "1.0"
 UPDATE_URL = "https://raw.githubusercontent.com/smutekkx/Orbit-Client-PL/refs/heads/main/Orbit%20Client.py"
 WEBHOOK_URL = "https://discord.com/api/webhooks/1517492582282821636/HOkRsg0_zGjuVkm6HsBJb24T0_E1uerbrBcGb1isjZ39KvGaL6JNG51x5P-t9aWR0PZN"
-BANNED_HWIDS = ["hwid"} 
+BANNED_HWIDS = [""} 
 # --------------------
 
 warnings.simplefilter('ignore', UserWarning)
